40f9de71b50b0917a0baa473b3ad7457fb701fb2
MetalStormGhost <ghosti@smytky.org>
2022-08-28 19:35:38 +0300

    Added missing remove_aircraft call in faction.py in case the T-45 mod is disabled when starting the campaign.

--- game/factions/faction.py
+++ game/factions/faction.py
@@ -286,6 +286,8 @@ class Faction:
         # aircraft
         if not mod_settings.a4_skyhawk:
             self.remove_aircraft("A-4E-C")
+        if not mod_settings.t45_goshawk:
+            self.remove_aircraft("T-45")
         if not mod_settings.hercules:
             self.remove_aircraft("Hercules")
         if not mod_settings.uh_60l:
